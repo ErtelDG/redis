762f87bfddcf19d62ba378d5f5f73121980ac24c
ErtelDG <dennis.ertel@t-online.de>
2025-09-22 20:33:22 +0200

    add style

--- public/client.js
+++ public/client.js
@@ -2,9 +2,14 @@ const ws = new WebSocket(`ws://${window.location.host}`);
 
 ws.onmessage = (event) => {
    const data = JSON.parse(event.data);
+
    document.getElementById("a").innerText = `${data.A}%`;
    document.getElementById("b").innerText = `${data.B}%`;
    document.getElementById("c").innerText = `${data.C}%`;
+
+   document.getElementById("barA").style.width = `${data.A}%`;
+   document.getElementById("barB").style.width = `${data.B}%`;
+   document.getElementById("barC").style.width = `${data.C}%`;
 };
 
 async function vote(option) {
